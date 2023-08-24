660b10cffca16c70f9741d4661725827f76c0ece
Anthony <86182786+MSFTScriptSculptor@users.noreply.github.com>
2023-08-25 00:21:58 +1300

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "location" {
+  description = "Azure region for resources"
+  default     = "East US"
+}
+
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  default     = "myResourceGroup"
+}
